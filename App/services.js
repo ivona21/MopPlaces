7b95a0dcb931b4401a76ea4171489faa8adc9a67
ivona21 <ivonamalekovic@hotmail.com>
2016-01-10 13:29:33 +0100

    images moved to cloudinary

--- App/services.js
+++ App/services.js
@@ -10,7 +10,7 @@
             {
                 Name: "Music Caffe",
                 Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-                ImageUrl: "../images/music-caffe.jpg",
+                ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_246/v1452427836/music-caffe_cvgonl.jpg",
                 Category: "Clubs",
                 Latitude: 43.854325,
                 Longitude: 18.390880
@@ -18,7 +18,7 @@
            {
                Name: "Sarajevo Taverna",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/sarajevo-taverna.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_232/v1452427834/sarajevo-taverna_wokph9.jpg",
                Category: "Clubs",
                Latitude: 43.853640,
                Longitude: 18.389964
@@ -26,7 +26,7 @@
            {
                Name: "Aquarius Vils",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/aquarius-vils.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_234/v1452427811/aquarius-vils_sbpttd.jpg",
                Category: "Clubs",
                Latitude: 43.854073,
                Longitude: 18.397952
@@ -34,7 +34,7 @@
            {
                Name: "Mr Charlie Chaplin",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/mr-charile-chaplin.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_276/v1452427823/mr-charile-chaplin_lgjapq.jpg",
                Category: "Clubs",
                Latitude: 43.850063,
                Longitude: 18.377295
@@ -42,7 +42,7 @@
            {
                Name: "Vatra",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/vatra.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_149/v1452427837/vatra_xw0xsj.jpg",
                Category: "Bars",
                Latitude: 43.851052,
                Longitude: 18.395025
@@ -50,7 +50,7 @@
            {
                Name: "Fashion",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/fashion.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_206/v1452427831/fashion_b1lg4q.jpg",
                Category: "Bars",
                Latitude: 43.852659,
                Longitude: 18.384779
@@ -58,7 +58,7 @@
            {
                Name: "Tito",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/tito.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_183/v1452427840/tito_d0eh5j.jpg",
                Category: "Bars",
                Latitude: 43.855530,
                Longitude: 18.402575
@@ -66,7 +66,7 @@
            {
                Name: "Johnny",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/johnny.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_232/v1452427814/johnny_s6jtbc.jpg",
                Category: "Bars",
                Latitude: 43.852484,
                Longitude: 18.384006
@@ -74,7 +74,7 @@
            {
                Name: "Biblioteka",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/biblioteka.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_290/v1452427831/biblioteka_hrsrwe.jpg",
                Category: "Bars",
                Latitude: 43.853713,
                Longitude: 18.382523
@@ -82,7 +82,7 @@
            {
                Name: "Montana",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/montana.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_228/v1452427821/montana_ybjixe.jpg",
                Category: "Food",
                Latitude: 43.855610,
                Longitude: 18.408419
@@ -90,7 +90,7 @@
            {
                Name: "Mutvak",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/mutvak.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_153/v1452427828/mutvak_obu8jr.jpg",
                Category: "Food",
                Latitude: 43.856194,
                Longitude: 18.408813
@@ -98,7 +98,7 @@
            {
                Name: "The Castle",
                Description: "Lorem ipsum dolor sit.",
-               ImageUrl: "../images/the-castle.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_228/v1452427837/the-castle_da0xjt.jpg",
                Category: "Food",
                Latitude: 43.854060,
                Longitude: 18.392567,
@@ -106,7 +106,7 @@
            {
                Name: "Element",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/element.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_228/v1452427838/element_txrpmm.jpg",
                Category: "Food",
                Latitude: 43.855146,
                Longitude: 18.399609
@@ -114,7 +114,7 @@
            {
                Name: "Mala kuca",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/mala-kuca.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_525/v1452427830/mala-kuca_c0jabk.jpg",
                Category: "Food",
                Latitude: 43.854204,
                Longitude: 18.389045
@@ -122,7 +122,7 @@
            {
                Name: "Mrkva Express",
                Description: "Lorem ipsum dolor sit amet, magna pulvinar.",
-               ImageUrl: "../images/mrkva-express.jpg",
+               ImageUrl: "http://res.cloudinary.com/dxkho18te/image/upload/c_scale,w_227/v1452427824/mrkva-express_wf3jkp.jpg",
                Category: "Food",
                Latitude: 43.854095,
                Longitude: 18.379873
